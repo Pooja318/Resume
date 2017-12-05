dcb95078d703bdb9df28f624aeca4c99523ed449
Pooja318 <das.pooja318@gmail.com>
2017-12-05 22:30:11 +0530

    first commit

---
+++ scripts/resume.js
@@ -0,0 +1,8 @@
+//function rate() {
+//    debugger;
+////    if(no<=3){
+//        var ele = create('<i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star-o" aria-hidden="true"></i><i class="fa fa-star-o" aria-hidden="true"></i>');
+//        console.log(ele);
+//        document.getElementById("rate").appendChild(ele);
+//
+//}
